8b5685f11940c0ced54f79e2fbc2728a6b616f13
3oraby <mohaahmed21102003@gmail.com>
2025-05-04 23:37:22 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 import 'package:gradution_project/core/Screens/splash1.dart';
-<<<<<<< HEAD
-import 'package:gradution_project/Authscreen.dart';
-=======
 import 'package:gradution_project/loginPAge.dart';
->>>>>>> 7646351b3fe249f6f82002d3032eb32d53d9ece2
 
 
 void main() async {
@@ -39,11 +35,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-<<<<<<< HEAD
-      home:  const Splash1(),
-=======
       home:  RegisterScreen(),
->>>>>>> 7646351b3fe249f6f82002d3032eb32d53d9ece2
     );
   }
 }
